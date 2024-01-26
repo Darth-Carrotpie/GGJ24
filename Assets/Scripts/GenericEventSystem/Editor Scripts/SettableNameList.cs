f60f68183d7be33ddff66bc8a7fe7c037c1b92a7
Darth-CarrotPie <carrotpie@gmail.com>
2024-01-26 22:57:58 +0200

    fix missing

---
+++ Assets/Scripts/GenericEventSystem/Editor Scripts/SettableNameList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+namespace GenericEventSystem {
+    [System.Serializable]
+    public class SettableNameList {
+        [SerializeField]
+        public List<string> list;
+        [SerializeField]
+        public List<int> indexes;
+
+        public bool Equals(SettableNameList other) {
+            return indexes.SequenceEqual(other.indexes);
+        }
+        public string ToString() {
+            string output = "";
+            foreach (string item in list) {
+                output += item + ", ";
+            }
+            return output;
+        }
+    }
+}
