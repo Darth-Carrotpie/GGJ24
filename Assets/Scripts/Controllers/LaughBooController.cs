06eb13e45096687fb8cb56acf9ab5d18d28c38e1
Qua <analizatoriuslx@gmail.com>
2024-01-27 17:52:03 +0200

    laughboocontroller chnages

--- Assets/Scripts/Controllers/LaughBooController.cs
+++ Assets/Scripts/Controllers/LaughBooController.cs
@@ -1,3 +1,4 @@
+using GenericEventSystem;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,14 +15,20 @@ public class LaughBooController : MonoBehaviour
     [SerializeField] AudioSource[] mainAudioSourceArray;
     [SerializeField] AudioClip[] mainLaughArray;
     [SerializeField] AudioMixer audioMixer;
+    [SerializeField] AudioMixerSnapshot[] snapshots;
+    [SerializeField] float[] weights;
+
+    [SerializeField] bool[] isTrackPlayingArray = new bool[5];
 
     [Range(0, 1f)]
     [SerializeField] float laughBooSlider = 0.5f;
 
-    private bool isPlayingTrack01;
+    [SerializeField] float timeToTransition = 0.5f;
 
     private void Start()
     {
+        //EventCoordinator.StartListening(EventName.World.CrowdStateChange(), Test);
+
         //if(instance == null)
         //    instance = this;
 
@@ -31,7 +38,7 @@ public class LaughBooController : MonoBehaviour
         //track01 = gameObject.AddComponent<AudioSource>();
         //track02 = gameObject.AddComponent<AudioSource>();
 
-    
+
         //for (int i = 0; i < mainAudioSourceArray.Length; i++)
         //{
         //    mainAudioSourceArray[i] = gameObject.AddComponent<AudioSource>();
@@ -42,8 +49,21 @@ public class LaughBooController : MonoBehaviour
         //    }   
         //}
 
-        audioMixer = GetComponent<AudioMixer>();
+        //audioMixer = GetComponent<AudioMixer>();
 
+        for (int i = 0; i < mainAudioSourceArray.Length; i++)
+        {
+            mainAudioSourceArray[i] = gameObject.AddComponent<AudioSource>();
+        }
+
+        for (int i = 0; i < isTrackPlayingArray.Length; i++)
+        {
+            isTrackPlayingArray[i] = false;
+        }
+
+        //audioMixer.TransitionToSnapshots(snapshots, 0.1f, 0.5f);
+
+        snapshots[2].TransitionTo(timeToTransition);
         laughBooLevel = LaughBooLevel.Boring;
         
        
@@ -51,7 +71,6 @@ public class LaughBooController : MonoBehaviour
 
     void Update()
     {
-        Debug.Log(isPlayingTrack01);
 
         if (laughBooSlider > 0.8f)
             laughBooLevel = LaughBooLevel.VeryFunny;
@@ -64,49 +83,69 @@ public class LaughBooController : MonoBehaviour
         else if (laughBooSlider < 0.2f)
             laughBooLevel = LaughBooLevel.VeryNotFunny;
 
-        ////if (laughBooLevel == LaughBooLevel.VeryFunny && track01.clip != mainLaughArray[0])
-        //if (laughBooLevel == LaughBooLevel.VeryFunny)
-        //{
+        if (laughBooLevel == LaughBooLevel.VeryFunny && !isTrackPlayingArray[0])
+        {
+            ResetWeights();
+            isTrackPlayingArray[0] = true;
+            //snapshots[0].TransitionTo(timeToTransition);
+            weights[0] = 1f;
+            audioMixer.TransitionToSnapshots(snapshots, weights, timeToTransition);
+        }
+        else if (laughBooLevel == LaughBooLevel.Funny && !isTrackPlayingArray[1])
+        {
+            ResetWeights();
+            isTrackPlayingArray[1] = true;
+            //snapshots[1].TransitionTo(timeToTransition);
+            weights[1] = 1f;
+            audioMixer.TransitionToSnapshots(snapshots, weights, timeToTransition);
+        }
+        else if (laughBooLevel == LaughBooLevel.Boring && !isTrackPlayingArray[2])
+        {
+            ResetWeights();
+            isTrackPlayingArray[2] = true;
+            //snapshots[2].TransitionTo(timeToTransition);
+            weights[2] = 1f;
+            audioMixer.TransitionToSnapshots(snapshots, weights, timeToTransition);
+        }
+        else if (laughBooLevel == LaughBooLevel.NotFunny && !isTrackPlayingArray[3])
+        {
+            ResetWeights();
+            isTrackPlayingArray[3] = true;
+            //snapshots[3].TransitionTo(timeToTransition);
+            weights[3] = 1f;
+            audioMixer.TransitionToSnapshots(snapshots, weights, timeToTransition);
+        }
+        else if (laughBooLevel == LaughBooLevel.VeryNotFunny && !isTrackPlayingArray[4])
+        {
+            ResetWeights();
+            isTrackPlayingArray[4] = true;
+            //snapshots[4].TransitionTo(timeToTransition);
+            weights[4] = 1f;
+            audioMixer.TransitionToSnapshots(snapshots, weights, timeToTransition);
+        }
+    }
 
-        //    //track01.Stop();
-        //    //track01.clip = mainLaughArray[0];
-        //    //track01.Play();
-        //    SwapTrack(mainLaughArray[0]);
+    void ResetWeights()
+    {
+        for (int i = 0; i < weights.Length; i++)
+        {
+            weights[i] = 0;
+        }
 
-        //}
-        //else if (laughBooLevel == LaughBooLevel.Funny && track01.clip != mainLaughArray[1])
-        //{
-        //    //track01.Stop();
-        //    //track01.clip = mainLaughArray[1];
-        //    //track01.Play();
-        //    SwapTrack(mainLaughArray[1]);
-        //}
-        //else if (laughBooLevel == LaughBooLevel.Boring && track01.clip != mainLaughArray[2])
-        //{
-        //    //track01.Stop();
-        //    //track01.clip = mainLaughArray[2];
-        //    //track01.Play();
-        //    SwapTrack(mainLaughArray[2]);
-        //}
-        //else if (laughBooLevel == LaughBooLevel.NotFunny && track01.clip != mainLaughArray[3])
-        //{
-        //    //track01.Stop();
-        //    //track01.clip = mainLaughArray[3];
-        //    //track01.Play();
-        //    SwapTrack(mainLaughArray[3]);
-        //}
-        //else if (laughBooLevel == LaughBooLevel.VeryNotFunny && track01.clip != mainLaughArray[4])
-        //{
-        //    //track01.Stop();
-        //    //track01.clip = mainLaughArray[4];
-        //    //track01.Play();
-        //    SwapTrack(mainLaughArray[4]);
-        //}
+        for (int i = 0; i < isTrackPlayingArray.Length; i++)
+        {
+            isTrackPlayingArray[i] = false;
+        }
+    }
+
+    void Test(GameMessage msg)
+    {
+        Debug.Log(msg.intMessage);
     }
 
     //public void SwapTrack(AudioClip newAudioClip)
     //{
-        
+
 
     //    StopAllCoroutines();
 
@@ -117,19 +156,19 @@ public class LaughBooController : MonoBehaviour
     //    //    track02.clip = newAudioClip;
     //    //    track01.Stop();
     //    //    track02.Play();
-            
+
     //    //}
     //    //else
     //    //{
     //    //    track01.clip = newAudioClip;
     //    //    track02.Stop();
     //    //    track01.Play();
-            
+
     //    //}
 
     //    isPlayingTrack01 = !isPlayingTrack01; 
     //}
-    
+
     //private IEnumerator FadeTrack(AudioClip newAudioClip)
     //{
     //    float timeToFade = 1.0f;
@@ -141,7 +180,7 @@ public class LaughBooController : MonoBehaviour
 
     //        track02.clip = newAudioClip;
     //        track02.Play();
-            
+
 
     //        while(timeElapsed < timeToFade)
     //        {
@@ -153,7 +192,7 @@ public class LaughBooController : MonoBehaviour
 
     //        track01.Stop();
 
-            
+
     //    }
     //    else
     //    {
