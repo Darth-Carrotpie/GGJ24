c0465635e34ee00fee624c9adf41de3ba9ca643d
Qua <analizatoriuslx@gmail.com>
2024-01-27 16:09:42 +0200

    added audio mixer and scene to test

--- Assets/Scripts/Controllers/LaughBooController.cs
+++ Assets/Scripts/Controllers/LaughBooController.cs
@@ -1,80 +1,175 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 
 public class LaughBooController : MonoBehaviour
 {
-    [SerializeField] AudioSource audioSource;
+    //public static LaughBooController instance;
+
+    enum LaughBooLevel { VeryFunny = 0, Funny = 1, Boring = 2, NotFunny = 3, VeryNotFunny = 4 };
+    LaughBooLevel laughBooLevel;
+
+    [SerializeField] AudioSource[] mainAudioSourceArray;
     [SerializeField] AudioClip[] mainLaughArray;
+    [SerializeField] AudioMixer audioMixer;
 
     [Range(0, 1f)]
     [SerializeField] float laughBooSlider = 0.5f;
 
-    private bool changeTrack = false;
+    private bool isPlayingTrack01;
 
-    private void Awake()
+    private void Start()
     {
-        audioSource = GetComponent<AudioSource>();
-    }
+        //if(instance == null)
+        //    instance = this;
 
-    void Start()
-    {
-        //audioSource = GetComponent<AudioSource>();
+        //track01 = GetComponent<AudioSource>();
+        //track02 = GetComponent<AudioSource>();
 
-        //audioSource.clip = mainLaughArray[2];
-        //audioSource.Play();
+        //track01 = gameObject.AddComponent<AudioSource>();
+        //track02 = gameObject.AddComponent<AudioSource>();
 
-        //audioSource.clip = boringLaughArray[Random.Range(0, boringLaughArray.Length)];
+    
+        for (int i = 0; i < mainAudioSourceArray.Length; i++)
+        {
+            mainAudioSourceArray[i] = gameObject.AddComponent<AudioSource>();
+            for (int p = 0; p < mainLaughArray.Length; p++)
+            {
+                if(i == p)
+                    mainAudioSourceArray[i].clip = mainLaughArray[p];
+            }   
+        }
+
+        audioMixer = GetComponent<AudioMixer>();
+
+        laughBooLevel = LaughBooLevel.Boring;
+        
+       
     }
 
     void Update()
     {
+        Debug.Log(isPlayingTrack01);
 
-        if (laughBooSlider > 0.8f && audioSource.clip != mainLaughArray[0])
+        if (laughBooSlider > 0.8f)
+            laughBooLevel = LaughBooLevel.VeryFunny;
+        else if (laughBooSlider < 0.8f && laughBooSlider > 0.6f)
+            laughBooLevel = LaughBooLevel.Funny;
+        else if (laughBooSlider < 0.6f && laughBooSlider > 0.4f)
+            laughBooLevel = LaughBooLevel.Boring;
+        else if (laughBooSlider < 0.4f && laughBooSlider > 0.2f)
+            laughBooLevel = LaughBooLevel.NotFunny;
+        else if (laughBooSlider < 0.2f)
+            laughBooLevel = LaughBooLevel.VeryNotFunny;
+
+        //if (laughBooLevel == LaughBooLevel.VeryFunny && track01.clip != mainLaughArray[0])
+        if (laughBooLevel == LaughBooLevel.VeryFunny)
         {
-            audioSource.Stop();
-            audioSource.clip = mainLaughArray[0];
-            audioSource.Play();
+            //track01.Stop();
+            //track01.clip = mainLaughArray[0];
+            //track01.Play();
+            SwapTrack(mainLaughArray[0]);
+
         }
-        else if (laughBooSlider < 0.8f && laughBooSlider > 0.6f && audioSource.clip != mainLaughArray[1])
+        else if (laughBooLevel == LaughBooLevel.Funny && track01.clip != mainLaughArray[1])
         {
-            audioSource.Stop();
-            audioSource.clip = mainLaughArray[1];
-            audioSource.Play();
+            //track01.Stop();
+            //track01.clip = mainLaughArray[1];
+            //track01.Play();
+            SwapTrack(mainLaughArray[1]);
         }
-        else if (laughBooSlider < 0.6f && laughBooSlider > 0.4f && audioSource.clip != mainLaughArray[2])
+        else if (laughBooLevel == LaughBooLevel.Boring && track01.clip != mainLaughArray[2])
         {
-            audioSource.Stop();
-            audioSource.clip = mainLaughArray[2];
-            audioSource.Play();
+            //track01.Stop();
+            //track01.clip = mainLaughArray[2];
+            //track01.Play();
+            SwapTrack(mainLaughArray[2]);
         }
-        else if (laughBooSlider < 0.4f && laughBooSlider > 0.2f && audioSource.clip != mainLaughArray[3])
+        else if (laughBooLevel == LaughBooLevel.NotFunny && track01.clip != mainLaughArray[3])
         {
-            audioSource.Stop();
-            audioSource.clip = mainLaughArray[3];
-            audioSource.Play();
+            //track01.Stop();
+            //track01.clip = mainLaughArray[3];
+            //track01.Play();
+            SwapTrack(mainLaughArray[3]);
         }
-        else if (laughBooSlider < 0.2f && audioSource.clip != mainLaughArray[4])
+        else if (laughBooLevel == LaughBooLevel.VeryNotFunny && track01.clip != mainLaughArray[4])
         {
-            audioSource.Stop();
-            audioSource.clip = mainLaughArray[4];
-            audioSource.Play();
+            //track01.Stop();
+            //track01.clip = mainLaughArray[4];
+            //track01.Play();
+            SwapTrack(mainLaughArray[4]);
         }
     }
 
-    //private void AddAudioSource(AudioClip audioClip)
-    //{
-    //    AudioSource audioSource = new AudioSource();
-    //    audioSource.clip = audioClip;
-    //    //audioSourceList.Add(audioSource);
-    //}
+    public void SwapTrack(AudioClip newAudioClip)
+    {
+        
 
-    //private void AudioFadeOut(List<AudioSource> audioSourceList)
-    //{
-    //    foreach (AudioSource audioSource in audioSourceList)
-    //    {
+        StopAllCoroutines();
 
-    //    }
-    //}
+        StartCoroutine(FadeTrack(newAudioClip));
+
+        //if (isPlayingTrack01)
+        //{
+        //    track02.clip = newAudioClip;
+        //    track01.Stop();
+        //    track02.Play();
+            
+        //}
+        //else
+        //{
+        //    track01.clip = newAudioClip;
+        //    track02.Stop();
+        //    track01.Play();
+            
+        //}
+
+        isPlayingTrack01 = !isPlayingTrack01; 
+    }
+    
+    private IEnumerator FadeTrack(AudioClip newAudioClip)
+    {
+        float timeToFade = 1.0f;
+        float timeElapsed = 0;
+
+        if (isPlayingTrack01)
+        {
+            //isPlayingTrack01 = !isPlayingTrack01;
+
+            track02.clip = newAudioClip;
+            track02.Play();
+            
+
+            while(timeElapsed < timeToFade)
+            {
+                track02.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                track01.volume = Mathf.Lerp(1, 2, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            track01.Stop();
+
+            
+        }
+        else
+        {
+            //isPlayingTrack01 = !isPlayingTrack01;
+
+            track01.clip = newAudioClip;
+            track01.Play();
+
+            while (timeElapsed < timeToFade)
+            {
+                track01.volume = Mathf.Lerp(0, 1, timeElapsed / timeToFade);
+                track02.volume = Mathf.Lerp(1, 2, timeElapsed / timeToFade);
+                timeElapsed += Time.deltaTime;
+                yield return null;
+            }
+
+            track02.Stop();
+        }
+    }
 }
