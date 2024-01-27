bf7d26f7881d0ccc483bc0b5c205290ae6d9336f
Kristupas Stumbrys <kristupas.stumbrys@gmail.com>
2024-01-27 19:56:14 +0200

    fix confliect in GameState

--- Assets/Scripts/GamePlay/GameState.cs
+++ Assets/Scripts/GamePlay/GameState.cs
@@ -1,17 +1,10 @@
 
 
-public enum GameState 
+public enum GameState
 {
     IntroScene,
     CharacterSelection,
-<<<<<<< HEAD
     BeatRun,
-=======
-    ForwardBeatRun,
-    // TODO: forward and reverse runs might be happening at the same time?
-    // TODO: remove?
-    ReverseBeatRun,
->>>>>>> 5aa5c8bc0ee7004094800c3b77b248142afea530
     PostLevelWin,
     PostLevelLose,
 }
